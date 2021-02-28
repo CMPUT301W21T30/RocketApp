7eb70663bc708281235be318505ed0a5fee4259e
Mike Greber <mikegreberdevelopment@gmail.com>
2021-02-28 12:51:51 -0700

    connect to firestore, implement DataManager and base classes

--- RocketApp/app/src/main/java/com/example/rocketapp/MainActivity.java
+++ RocketApp/app/src/main/java/com/example/rocketapp/MainActivity.java
@@ -10,5 +10,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        DataManager.createUser(new User("Morty"), user -> {
+            DataManager.publishExperiment(
+                    new Experiment(user.getId(), "Delayed Experiment", "This is a new experiment", "Alberta", 10, false),
+                    experiment -> {
+                        DataManager.push(new Trial("This is a new trial", user), experiment, null);
+                        DataManager.push(new Question("This is a new question", user),  experiment, null);
+                    });
+        });
+
+
+
     }
 }
