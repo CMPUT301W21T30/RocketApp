6487891677ae41a5e13193bbf0c7485eebe447e3
qasemsai <70786752+qasemsai@users.noreply.github.com>
2021-03-07 22:58:24 -0700

    added ExperimentDialog

---
+++ RocketApp/app/src/main/java/com/example/rocketapp/ExperimentDialog.java
@@ -0,0 +1,140 @@
+package com.example.rocketapp;
+
+import android.content.Context;
+import android.os.Build;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
+import androidx.fragment.app.DialogFragment;
+
+import java.util.Objects;
+
+public class ExperimentDialog extends DialogFragment {
+
+    //Forces implementing class to create a function that handles experiment returned from fragment
+    public interface OnInputListener{
+        void returnExperiment(Experiment exp);
+    }
+
+    public OnInputListener dialogListener;
+
+    private static final String TAG = "ExperimentDialog";
+
+    private EditText descriptionET, regionET, minTrialsET;
+    private Button okButton, cancelButton;
+    private CheckBox geoBox;
+    private Boolean geolocationEnabled;
+    private Spinner expType;
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState){
+        View view = inflater.inflate(R.layout.experiment_dialog, container, false);
+        initialSetup(view);
+
+        geoBox.setOnClickListener(v -> {
+            if (geoBox.isChecked()){
+                geolocationEnabled = true;
+                Toast.makeText(Objects.requireNonNull(getActivity()).getApplicationContext(), "Geolocation Enabled", Toast.LENGTH_LONG).show();
+            } else {
+                geolocationEnabled = false;
+            }
+        });
+
+        okButton.setOnClickListener(v -> {
+            if (checkInputsValid()) {
+                Log.d(TAG, "onClick: capturing input");
+                if (dialogListener != null) dialogListener.returnExperiment(getExperiment(expType.getSelectedItem().toString()));
+                Objects.requireNonNull(getDialog()).dismiss();
+            }
+        });
+
+        cancelButton.setOnClickListener(v -> {
+            Log.d(TAG, "onClick: Closing Dialog");
+            Objects.requireNonNull(getDialog()).dismiss();
+        });
+
+        return view;
+    }
+
+    public void initialSetup(View view){
+
+        //Initializing dialog attributes
+        //Sources:
+        //How to add a checkbox: https://developer.android.com/guide/topics/ui/controls/checkbox
+        //how to add a dropdown list: https://developer.android.com/guide/topics/ui/controls/spinner
+
+        descriptionET = view.findViewById(R.id.description_input);
+        regionET = view.findViewById(R.id.region_input);
+        minTrialsET = view.findViewById(R.id.min_trial);
+        geoBox = view.findViewById(R.id.geolocation);
+        okButton = view.findViewById(R.id.add_exp);
+        cancelButton = view.findViewById(R.id.cancel_exp);
+        expType = view.findViewById(R.id.select_exp);
+        ArrayAdapter<String> myAdapter = new ArrayAdapter<>(getContext(),
+                android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.experiments));
+        myAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        expType.setAdapter(myAdapter);
+    }
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        try{
+            dialogListener = (OnInputListener) getActivity();
+        }catch(ClassCastException e){
+            Log.e(TAG, "onAttach: ClassCastException: " + e.getMessage());
+        }
+    }
+
+    public Experiment getExperiment(String type){
+
+        //given a string describing the experiment type, returns an experiment of said type based on user inputs
+        Experiment exp;
+        switch(type) {
+            //TODO: deal with name
+            case ("Binomial Experiment"):
+                exp = new BinomialExperiment("memes", descriptionET.getText().toString(),
+                        regionET.getText().toString(), Integer.parseInt(minTrialsET.getText().toString()), geolocationEnabled);
+                return exp;
+            case ("Count Experiment"):
+                exp = new CountExperiment("memes", descriptionET.getText().toString(),
+                        regionET.getText().toString(), Integer.parseInt(minTrialsET.getText().toString()), geolocationEnabled);
+                return exp;
+            case ("Integer Count Experiment"):
+                exp = new IntCountExperiment("memes", descriptionET.getText().toString(),
+                        regionET.getText().toString(), Integer.parseInt(minTrialsET.getText().toString()), geolocationEnabled);
+                return exp;
+            case ("Measurement Experiment"):
+                exp = new MeasurementExperiment("memes", descriptionET.getText().toString(),
+                        regionET.getText().toString(), Integer.parseInt(minTrialsET.getText().toString()), geolocationEnabled);
+                return exp;
+            default:
+                exp = new BinomialExperiment("notMemes", descriptionET.getText().toString(),
+                        regionET.getText().toString(), Integer.parseInt(minTrialsET.getText().toString()), geolocationEnabled);
+                return exp;
+
+        }
+    }
+
+    private boolean checkInputsValid() {
+        return  Validate.lengthInRange(descriptionET, 1, 40, true) &&
+                Validate.intInRange(minTrialsET, 0, Integer.MAX_VALUE, true) &&
+                Validate.lengthInRange(regionET, 1, 40, true)
+                ;
+    }
+}
--- RocketApp/app/src/main/java/com/example/rocketapp/ExperimentsListActivity.java
+++ RocketApp/app/src/main/java/com/example/rocketapp/ExperimentsListActivity.java
@@ -3,9 +3,11 @@ package com.example.rocketapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
-public class ExperimentsListActivity extends AppCompatActivity {
+public class ExperimentsListActivity extends AppCompatActivity implements ExperimentDialog.OnInputListener{
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +18,12 @@ public class ExperimentsListActivity extends AppCompatActivity {
         textView.setText(DataManager.getUser().getName());
         TextView textView2 = findViewById(R.id.textView2);
         textView2.setText(String.valueOf(DataManager.getIsOwner()));
+    }
+
+
+    @Override
+    public void returnExperiment(Experiment exp) {
+        Log.d("ExperimentsListActivity", "sendExperiment: got the experiment" + exp.info.getDescription());
+        //TODO: handle received experiment
     }
 }
