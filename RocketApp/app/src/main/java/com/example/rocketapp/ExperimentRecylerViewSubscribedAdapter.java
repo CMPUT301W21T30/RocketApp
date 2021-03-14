4e2f2d54c81cf6d70b4284a7f6c87c22327fe55d
Trevor Liu <trevorcliu@hotmail.com>
2021-03-14 16:58:24 -0700

    Added Swipe to publish for Owner but unsuessfully

--- RocketApp/app/src/main/java/com/example/rocketapp/ExperimentRecylerViewSubscribedAdapter.java
+++ RocketApp/app/src/main/java/com/example/rocketapp/ExperimentRecylerViewSubscribedAdapter.java
@@ -1,34 +1,28 @@
 package com.example.rocketapp;
 
-import android.annotation.SuppressLint;
 import android.content.Context;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
-import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.android.material.card.MaterialCardView;
 
-import org.w3c.dom.Text;
-
 import java.util.ArrayList;
 
-public class ExperimentRecylerViewAdapter extends RecyclerView.Adapter<ExperimentRecylerViewAdapter.ViewHolder> {
+public class ExperimentRecylerViewSubscribedAdapter extends RecyclerView.Adapter<ExperimentRecylerViewSubscribedAdapter.ViewHolder> {
 
     private static final String TAG = "ExperimentRecylerViewAd";
 
     private ArrayList<Experiment> experiments;
     private Context context;
 
-    public ExperimentRecylerViewAdapter(Context context, ArrayList<Experiment> experiments) {
+    public ExperimentRecylerViewSubscribedAdapter(Context context, ArrayList<Experiment> experiments) {
         this.experiments = experiments;
         this.context = context;
     }
--- RocketApp/app/src/main/java/com/example/rocketapp/ExperimentsListActivity.java
+++ RocketApp/app/src/main/java/com/example/rocketapp/ExperimentsListActivity.java
@@ -1,30 +1,29 @@
 package com.example.rocketapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.ItemTouchHelper;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.provider.ContactsContract;
 import android.util.Log;
-import android.widget.TextView;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ImageButton;
-import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
-import java.util.LinkedHashSet;
-import java.util.Set;
-import java.util.concurrent.TimeUnit;
 
 public class ExperimentsListActivity extends AppCompatActivity implements ExperimentDialog.OnInputListener {
 
     //use this button to navigate to the profile page of the user
     private static final String TAG = "ExperimentsListActivity";
     public ImageButton profileBtn;
+    ArrayList<Experiment> experimentsOwned;
+    ArrayList<Experiment> experimentsSubscribed;
+    ExperimentRecyclerViewOwnedAdapter adapterOwned;
+    ExperimentRecylerViewSubscribedAdapter adapteSubscribedr;
 
 
     @Override
@@ -32,12 +31,9 @@ public class ExperimentsListActivity extends AppCompatActivity implements Experi
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_experiments_list);
 
-        ArrayList<Experiment> experimentsOwned = DataManager.getOwnedExperimentsArrayList();
-        initRecyclerViewOwner(experimentsOwned);
+        initRecyclerViewOwner();
 
-        // Display Subscribed Experiment
-        ArrayList<Experiment> experiments = DataManager.getSubscribedExperimentArrayList();
-        initRecyclerView(experiments);
+        initRecyclerViewSubscribed();
 
 
         profileBtn = findViewById(R.id.profile_button);
@@ -45,42 +41,77 @@ public class ExperimentsListActivity extends AppCompatActivity implements Experi
             Intent userProfileIntent = new Intent(getApplicationContext(), UserProfileActivity.class);
             startActivity(userProfileIntent);
 
+//            DataManager.publishExperiment(new CountExperiment("2nd Experiment", "BC", 20, true), (s) -> {}, (e) -> {});
+
         });
     }
 
-    private void initRecyclerViewOwner(ArrayList<Experiment> experimentsOwned) {
+    /**
+     * Update the the Experiments List every time it back from other fragment
+     */
+    @Override
+    protected void onResume() {
+        super.onResume();
+        initRecyclerViewOwner();
+        initRecyclerViewSubscribed();
+    }
+
+    private void initRecyclerViewOwner() {
         Log.d(TAG, "initRecyclerView: init recyclerview.");
 
         RecyclerView experimentRecyclerView = findViewById(R.id.experimentRecyclerViewOwner);
 
-        // You can pass in a flag to the constructor
-        ExperimentRecyclerViewOwnedAdapter adapter = new ExperimentRecyclerViewOwnedAdapter(this, experimentsOwned);
-        experimentRecyclerView.setAdapter(adapter);
+        experimentsOwned = DataManager.getOwnedExperimentsArrayList();
+        adapterOwned = new ExperimentRecyclerViewOwnedAdapter(this, experimentsOwned);
+        experimentRecyclerView.setAdapter(adapterOwned);
         experimentRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        // Set up Swipe Gesture for Published and UnPublished
+        ItemTouchHelper itemTouchHelperOwned = new ItemTouchHelper(simpleCallback);
+        itemTouchHelperOwned.attachToRecyclerView(experimentRecyclerView);
     }
 
-    private void initRecyclerView(ArrayList<Experiment> experiments){
+    private void initRecyclerViewSubscribed(){
         Log.d(TAG, "initRecyclerView: init recyclerview.");
+        RecyclerView experimentRecyclerView = findViewById(R.id.experimentRecyclerViewSubscribed);
 
-        RecyclerView experimentRecyclerView = findViewById(R.id.experimentRecyclerView);
-
-        // You can pass in a flag to the constructor
-        ExperimentRecylerViewAdapter adapter = new ExperimentRecylerViewAdapter(this, experiments);
-        experimentRecyclerView.setAdapter(adapter);
+        experimentsSubscribed = DataManager.getSubscribedExperimentArrayList();
+        adapteSubscribedr = new ExperimentRecylerViewSubscribedAdapter(this, experimentsSubscribed);
+        experimentRecyclerView.setAdapter(adapteSubscribedr);
         experimentRecyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
+
+
+
+    ItemTouchHelper.SimpleCallback simpleCallback = new ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT) {
+        @Override
+        public boolean onMove(@NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, @NonNull RecyclerView.ViewHolder target) {
+            return false;
+        }
+
+        @Override
+        public void onSwiped(@NonNull RecyclerView.ViewHolder viewHolder, int direction) {
+            int position = viewHolder.getAdapterPosition();
+
+            switch (direction) {
+                case ItemTouchHelper.LEFT:
+                    DataManager.publishExperiment(experimentsOwned.get(position), (s) -> {}, (e) -> {});
+                    Toast.makeText(getApplicationContext(), "Published " + experimentsOwned.get(position).info.getDescription(), Toast.LENGTH_SHORT).show();
+                    adapterOwned.notifyDataSetChanged();
+                    break;
+                case ItemTouchHelper.RIGHT:
+                    DataManager.unpublishExperiment(experimentsOwned.get(position), (s) -> {}, (e) -> {});
+                    Toast.makeText(getApplicationContext(), "UnPublished " + experimentsOwned.get(position).info.getDescription(), Toast.LENGTH_SHORT).show();
+                    adapterOwned.notifyDataSetChanged();
+                    break;
+            }
+
+        }
+    };
 
     @Override
     public void returnExperiment(Experiment exp) {
         Log.d("ExperimentsListActivity", "sendExperiment: got the experiment" + exp.info.getDescription());
         //TODO: handle received experiment
     }
-            /*
-        loginBtn = findViewById(R.id.loginBtn);
-        loginBtn.setOnClickListener(v -> {
-        EditText usernameEditText = findViewById(R.id.usernameEditText);
-        String username = usernameEditText.getText().toString();
-        loginOrCreateUser(username, isOwner.get());
-    });
-    */
 }
