17c5eb73af53b9adde960c1292fab45ee765f74f
Mike Greber <mikegreberdevelopment@gmail.com>
2021-03-17 10:13:23 -0600

    removed old experiments

--- RocketApp/app/src/main/java/com/example/rocketapp/ExperimentsListActivity.java
+++ RocketApp/app/src/main/java/com/example/rocketapp/ExperimentsListActivity.java
@@ -77,7 +77,6 @@ public class ExperimentsListActivity extends AppCompatActivity{
 
         experimentsOwned = DataManager.getOwnedExperimentsArrayList();
         adapterOwned = new ExperimentListAdapter(experimentsOwned, experiment -> {
-//            Intent intent = new Intent(this, DataManager.activityClassMap.get(experiment.getType()));
             Intent intent = new Intent(this, ExperimentActivity.class);
             intent.putExtra("id", experiment.getId());
             startActivity(intent);
@@ -97,7 +96,6 @@ public class ExperimentsListActivity extends AppCompatActivity{
         RecyclerView experimentRecyclerView = findViewById(R.id.experimentRecyclerViewSubscribed);
         experimentsSubscribed = DataManager.getSubscribedExperimentArrayList();
         adapterSubscribed = new ExperimentListAdapter(experimentsSubscribed, experiment -> {
-//            Intent intent = new Intent(this, DataManager.activityClassMap.get(experiment.getType()));
             Intent intent = new Intent(this, ExperimentActivity.class);
             intent.putExtra("id", experiment.getId());
             startActivity(intent);
