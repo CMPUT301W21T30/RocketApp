93a29bed3735490d9644b7be6735b988b6b3f35f
Mike Greber <mikegreberdevelopment@gmail.com>
2021-03-17 10:03:54 -0600

    refactor

--- RocketApp/app/src/main/java/com/example/rocketapp/ExperimentActivity.java
+++ RocketApp/app/src/main/java/com/example/rocketapp/ExperimentActivity.java
@@ -12,18 +12,19 @@ import android.widget.Toast;
 public class ExperimentActivity extends AppCompatActivity {
 
     private Experiment experiment;
+    private TextView meanView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_experiment);
-        TextView meanView = findViewById(R.id.meanView);
+        meanView = findViewById(R.id.meanView);
         TextView meanText = findViewById(R.id.meanText);
         experiment = DataManager.getExperiment(getIntent().getSerializableExtra("id"));
-        if(experiment.getType().equals("Binomial")){
+        if (experiment.getType().equals("Binomial")) {
             meanText.setText("Success Ratio - x");
         }
-        getMean(experiment, meanView);
+//        getMean(experiment, meanView);
         TextView experimentType = findViewById(R.id.experimentTypeTextView);
         experimentType.setText(experiment.getType());
 
@@ -42,66 +43,18 @@ public class ExperimentActivity extends AppCompatActivity {
                 });
             }).show(getSupportFragmentManager(), "ADD_TRIAL");
         });
+
+        DataManager.listen(experiment, this::update);
     }
 
-    private void getMean(Experiment experiment, TextView meanView) {
-        DataManager.listen(experiment, updatedExperiment -> {
-            if (updatedExperiment.getType().equals("Count")) {
-                float mean = 0;
-                for (int j = 0; j < updatedExperiment.getTrials().size(); j++) {
-                    CountTrial t = (CountTrial) updatedExperiment.getTrials().get(j);
-                    System.out.println(t.getCount());
-                    mean = mean + t.getCount();
-                }
-                if (updatedExperiment.getTrials().size() > 0) {
-                    meanView.setText(String.valueOf(mean / updatedExperiment.getTrials().size()));
-                } else {
-                    meanView.setText("0.0");
-                }
-            } else if (updatedExperiment.getType().equals("IntCount")) {
-                float mean = 0;
-                for (int j = 0; j < updatedExperiment.getTrials().size(); j++) {
-                    IntCountTrial t = (IntCountTrial) updatedExperiment.getTrials().get(j);
-                    mean = mean + t.getPCount();
-                }
-                if (updatedExperiment.getTrials().size() > 0) {
-                    meanView.setText(String.valueOf(mean / updatedExperiment.getTrials().size()));
-                } else {
-                    meanView.setText("0.0");
-                }
-            }
-            else if (updatedExperiment.getType().equals("Measurement")) {
-                float mean = 0;
-                for (int j = 0; j < updatedExperiment.getTrials().size(); j++) {
-                    MeasurementTrial t = (MeasurementTrial) updatedExperiment.getTrials().get(j);
-                    mean = mean + t.getMeasurement();
-                }
-                if (updatedExperiment.getTrials().size() > 0) {
-                    meanView.setText(String.valueOf(mean / updatedExperiment.getTrials().size()));
-                } else {
-                    meanView.setText("0.0");
-                }
-            }
-            else{
-                int success = 0;
-                int fail = 0;
-                for(int j=0; j<updatedExperiment.getTrials().size(); j++){
-                    BinomialTrial t = (BinomialTrial) updatedExperiment.getTrials().get(j);
-                    if(t.isValue()){
-                        success++;
-                    }
-                    else{
-                        fail++;
-                    }
-                }
-                if((success+fail)>0){
-                    meanView.setText(String.valueOf(success/(success+fail)));
-                }
-                else{
-                    meanView.setText("0");
-                }
-            }
+    void update(Experiment experiment) {
+        // Could add all updates here
+        try {
+            // TODO getMean() and other stats functions will try to divide by zero, need to fix those functions to return a default case when no trials exist, then we don't need this to be in a try catch
+            meanView.setText(String.valueOf(experiment.getMean()));
+        } catch (Exception e) {
+            meanView.setText("0.0");
+        }
 
-        });
     }
 }
