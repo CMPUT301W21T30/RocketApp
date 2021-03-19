7737db2d49309ae63d2e6c472ed75fbb005f19a8
Mike Greber <mikegreberdevelopment@gmail.com>
2021-03-19 10:01:21 -0600

    Update ExperimentUnitTest.java

--- RocketApp/app/src/test/java/com/example/rocketapp/ExperimentUnitTest.java
+++ RocketApp/app/src/test/java/com/example/rocketapp/ExperimentUnitTest.java
@@ -1,28 +1,13 @@
 package com.example.rocketapp;
 
 import org.junit.Test;
-
 import java.util.ArrayList;
-
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
 
 public class ExperimentUnitTest {
-    @Test
-    public void checkExperimentIsValid() {
-        Experiment experiment = new BinomialExperiment();
-        assertFalse(experiment.isValid());
-    }
 
-    @Test
-    public void checkExperimentOwnerIsValid() {
-        Experiment experiment = new BinomialExperiment();
-        assertFalse(experiment.ownerIsValid());
-    }
-
-    @Test
-    public void checkExperimentGetTrials() {
+    private Experiment createMockExperiment() {
         Experiment experiment = new BinomialExperiment();
         ArrayList<BinomialTrial> trials = new ArrayList<>();
         trials.add(new BinomialTrial(true));
@@ -30,19 +15,37 @@ public class ExperimentUnitTest {
         trials.add(new BinomialTrial(true));
         trials.add(new BinomialTrial(false));
         experiment.setTrials(trials);
+        ArrayList<Question> questions = new ArrayList<>();
+        questions.add(new Question("1st question"));
+        questions.add(new Question("2nd question"));
+        questions.add(new Question("3rd question"));
+        questions.add(new Question("4th question"));
+        experiment.setQuestions(questions);
+        return experiment;
+    }
+
+    @Test
+    public void checkExperimentIsValid() {
+        Experiment experiment = createMockExperiment();
+        assertFalse(experiment.isValid());
+    }
+
+    @Test
+    public void checkExperimentOwnerIsValid() {
+        Experiment experiment = createMockExperiment();
+        assertFalse(experiment.ownerIsValid());
+    }
+
+    @Test
+    public void checkExperimentGetTrials() {
+        Experiment experiment = createMockExperiment();
         assertEquals(4, experiment.getTrials().size());
     }
 
     @Test
     public void checkExperimentGetQuestions() {
-        Experiment experiment = new BinomialExperiment();
-        ArrayList<Question> questions = new ArrayList<>();
-        questions.add(new Question("Question 1"));
-        questions.add(new Question("Question 2"));
-        questions.add(new Question("Question 3"));
-        questions.add(new Question("Question 4"));
-        experiment.setQuestions(questions);
-        assertEquals(4, experiment.getTrials().size());
+        Experiment experiment = createMockExperiment();
+        assertEquals(4, experiment.getQuestions().size());
     }
 
 }
