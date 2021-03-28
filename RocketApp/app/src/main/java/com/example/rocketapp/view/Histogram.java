9c3914ba1e7c2bcd1c1d526e28311f6e3ee19e31
qasemsai <70786752+qasemsai@users.noreply.github.com>
2021-03-28 13:53:06 -0600

    histogram update

--- RocketApp/app/src/main/java/com/example/rocketapp/view/Histogram.java
+++ RocketApp/app/src/main/java/com/example/rocketapp/view/Histogram.java
@@ -6,11 +6,28 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.rocketapp.R;
+import com.example.rocketapp.controller.ExperimentManager;
+import com.example.rocketapp.model.experiments.Experiment;
+import com.example.rocketapp.model.trials.Trial;
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.data.BarEntry;
+
+import java.util.ArrayList;
 
 public class Histogram extends AppCompatActivity {
+
+    BarChart barChart; //Source: https://www.youtube.com/watch?v=pi1tq-bp7uA
+    Experiment experiment;
+
     @Override
     protected void onCreate (Bundle savedInstanceState){
         super.onCreate (savedInstanceState);
         setContentView(R.layout.graph_activity);
+
+        barChart = (BarChart)findViewById(R.id.histogram);
+        ArrayList<BarEntry> barEntries = new ArrayList<BarEntry>();
+
+        experiment = ExperimentManager.getExperiment(getIntent().getSerializableExtra("id"));
+        ArrayList<Trial> trials = (ArrayList<Trial>) experiment.getTrials();
     }
 }
