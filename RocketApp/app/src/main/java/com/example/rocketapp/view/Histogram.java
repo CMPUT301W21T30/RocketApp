441c16776e8f5846fbdb5b4bbe1c6206162bf0fb
archit-soni <43818375+archit-soni@users.noreply.github.com>
2021-03-29 02:33:13 +0530

    histogram added

--- RocketApp/app/src/main/java/com/example/rocketapp/view/Histogram.java
+++ RocketApp/app/src/main/java/com/example/rocketapp/view/Histogram.java
@@ -37,28 +37,40 @@ public class Histogram extends AppCompatActivity {
 
         experiment = ExperimentManager.getExperiment(getIntent().getSerializableExtra("id"));
         System.out.println(experiment);
-        ArrayList<Trial> trials = (ArrayList<Trial>) experiment.getTrials();
-        for (int i = 0; i < experiment.getTrials().size(); i++) {
-            System.out.println(experiment.getTrials().get(i));
+        ArrayList<Trial> trials = (ArrayList<Trial>) experiment.getFilteredTrials();
+        for (int i = 0; i < experiment.getFilteredTrials().size(); i++) {
+            System.out.println(experiment.getFilteredTrials().get(i));
             int freq = 0;
-            for (int j = i; j < experiment.getTrials().size(); j++) {
-                System.out.println(experiment.getTrials().get(j));
-                int found = 0;
-                for (int k = 0; k < done.size(); k++) {
-                    if (experiment.getTrials().get(j).getId() == (done.get(k).getId())) {
-                        found = found + 1;
-                    }
-                    if (found == 0) {
-                        if (experiment.getTrials().get(i).getValueString() == experiment.getTrials().get(j).getValueString()) {
-                            freq = freq + 1;
-                            done.add(experiment.getTrials().get(j));
-                        }
-                    }
+            int found = 0;
+            for (int k = 0; k < done.size(); k++) {
+                if (experiment.getFilteredTrials().get(i).getId() == (done.get(k).getId())) {
+                    found = found + 1;
                 }
             }
-            System.out.println(experiment.getTrials().get(i).getValueString());
-            System.out.println(freq);
-            barEntries.add(new BarEntry(Float.parseFloat(experiment.getTrials().get(i).getValueString()), freq));
+            System.out.println("FOUND");
+            System.out.println(found);
+            if (found == 0) {
+                for (int j = i; j < experiment.getFilteredTrials().size(); j++) {
+                    System.out.println(experiment.getFilteredTrials().get(j));
+                    System.out.println("YOYOYO");
+                    System.out.println(experiment.getFilteredTrials().get(i).getValueString());
+                    System.out.println(experiment.getFilteredTrials().get(j).getValueString());
+                    if (experiment.getFilteredTrials().get(i).getValueString().equals(experiment.getFilteredTrials().get(j).getValueString())) {
+                        System.out.println("ABCD");
+                        freq = freq + 1;
+                        done.add(experiment.getFilteredTrials().get(j));
+                    }
+                }
+                if (experiment.getFilteredTrials().get(i).getValueString().equals("True")) {
+                    barEntries.add(new BarEntry(Float.parseFloat("1"), freq));
+                }
+                else if(experiment.getFilteredTrials().get(i).getValueString().equals("False")){
+                    barEntries.add(new BarEntry(Float.parseFloat("0"), freq));
+                }
+                else{
+                    barEntries.add(new BarEntry(Float.parseFloat(experiment.getFilteredTrials().get(i).getValueString()), freq));
+                }
+            }
         }
         BarDataSet barDataSet = new BarDataSet(barEntries, "Trials");
         barDataSet.setColors(ColorTemplate.MATERIAL_COLORS);
