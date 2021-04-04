16f6e8c4a83063909ecc3a4a022a17ea1a38cab9
Aryan <aryan.joy@outlook.com>
2021-04-04 15:46:25 -0600

    encoded description and trial into qrcode

--- RocketApp/app/src/main/java/com/example/rocketapp/view/activities/GenerateQRcodeActivity.java
+++ RocketApp/app/src/main/java/com/example/rocketapp/view/activities/GenerateQRcodeActivity.java
@@ -12,6 +12,7 @@ import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.ActionBar;
@@ -21,6 +22,7 @@ import com.example.rocketapp.R;
 import com.example.rocketapp.controller.ExperimentManager;
 import com.example.rocketapp.model.experiments.BinomialExperiment;
 import com.example.rocketapp.model.experiments.Experiment;
+import com.example.rocketapp.model.trials.BinomialTrial;
 import com.google.zxing.BarcodeFormat;
 import com.google.zxing.MultiFormatWriter;
 import com.google.zxing.WriterException;
@@ -37,6 +39,7 @@ public class GenerateQRcodeActivity extends AppCompatActivity {
     private CheckBox registerpass;
     private CheckBox registerfail;
     private TextView experimentType;
+    private TextView checkGenerate;
 
     @Override
     protected void onCreate(Bundle saveInstanceState){
@@ -46,6 +49,8 @@ public class GenerateQRcodeActivity extends AppCompatActivity {
         registerpass = findViewById(R.id.registerfail2);
         experimentType = findViewById(R.id.experimentType2);
         trialsEditText = findViewById(R.id.trialEditText2);
+
+        checkGenerate = findViewById(R.id.checkGenerate);
 
         experiment = ExperimentManager.getExperiment(getIntent().getSerializableExtra("id"));
         experiment = ExperimentManager.getExperiment(getIntent().getSerializableExtra("id"));
@@ -73,25 +78,83 @@ public class GenerateQRcodeActivity extends AppCompatActivity {
             @RequiresApi(api = Build.VERSION_CODES.M)
             @Override
             public void onClick(View v) {
-                //Get input value from edit text
-                String sText = experiment.info.getDescription() ;
-                //Initialize multi format writer
-                MultiFormatWriter writer = new MultiFormatWriter();
-                try {
-                    //Initialize bit matrix
-                    BitMatrix matrix = writer.encode(sText, BarcodeFormat.QR_CODE, 800,800);
-                    //Initialize barcode encoder
-                    BarcodeEncoder encoder = new BarcodeEncoder();
-                    //Initialize bitmap
-                    Bitmap bitmap = encoder.createBitmap(matrix);
-                    //Set bitmap on image view
-                    qrcode.setImageBitmap(bitmap);
+                if (experiment.getType().equals(BinomialTrial.TYPE)){
+                    if(registerpass.isChecked() && !(registerfail.isChecked())){
+
+                        String sText = experiment.info.getDescription() + experiment.info.getRegion() + "pass";
+                        //Initialize multi format writer
+                        MultiFormatWriter writer = new MultiFormatWriter();
+                        try {
+                            //Initialize bit matrix
+                            BitMatrix matrix = writer.encode(sText, BarcodeFormat.QR_CODE, 800,800);
+                            //Initialize barcode encoder
+                            BarcodeEncoder encoder = new BarcodeEncoder();
+                            //Initialize bitmap
+                            Bitmap bitmap = encoder.createBitmap(matrix);
+                            //Set bitmap on image view
+                            qrcode.setImageBitmap(bitmap);
 
 
 
-                } catch (WriterException e) {
-                    e.printStackTrace();
+                        } catch (WriterException e) {
+                            e.printStackTrace();
+                        }
+                        checkGenerate.setText(sText);
+                        giveConfirmation();
+
+                    }
+                    else if (registerfail.isChecked() && !(registerpass.isChecked())){
+                        String sText = experiment.info.getDescription() + experiment.info.getRegion() + "fail";
+                        //Initialize multi format writer
+                        MultiFormatWriter writer = new MultiFormatWriter();
+                        try {
+                            //Initialize bit matrix
+                            BitMatrix matrix = writer.encode(sText, BarcodeFormat.QR_CODE, 800,800);
+                            //Initialize barcode encoder
+                            BarcodeEncoder encoder = new BarcodeEncoder();
+                            //Initialize bitmap
+                            Bitmap bitmap = encoder.createBitmap(matrix);
+                            //Set bitmap on image view
+                            qrcode.setImageBitmap(bitmap);
+
+
+
+                        } catch (WriterException e) {
+                            e.printStackTrace();
+                        }
+                        checkGenerate.setText(sText);
+                        giveConfirmation();
+                    }
+                    else{
+                        //cannot select both
+                        giveError();
+                    }
                 }
+                else {
+                    String sText = experiment.info.getDescription() + experiment.info.getRegion() + trialsEditText.getText().toString();
+                    //Initialize multi format writer
+                    MultiFormatWriter writer = new MultiFormatWriter();
+                    try {
+                        //Initialize bit matrix
+                        BitMatrix matrix = writer.encode(sText, BarcodeFormat.QR_CODE, 800,800);
+                        //Initialize barcode encoder
+                        BarcodeEncoder encoder = new BarcodeEncoder();
+                        //Initialize bitmap
+                        Bitmap bitmap = encoder.createBitmap(matrix);
+                        //Set bitmap on image view
+                        qrcode.setImageBitmap(bitmap);
+
+
+
+                    } catch (WriterException e) {
+                        e.printStackTrace();
+                    }
+                    checkGenerate.setText(sText);
+                    giveConfirmation();
+
+                }
+
+                //Get input value from edit text
 
             }
         });
@@ -110,5 +173,14 @@ public class GenerateQRcodeActivity extends AppCompatActivity {
         }
         return super.onOptionsItemSelected(item);
     }
+
+    public void giveError(){
+        Toast.makeText(this, "Cannot do this", Toast.LENGTH_LONG).show();
+    }
+
+    public void giveConfirmation(){
+        Toast.makeText(this, "Generated!", Toast.LENGTH_LONG).show();
+    }
+
 
 }
